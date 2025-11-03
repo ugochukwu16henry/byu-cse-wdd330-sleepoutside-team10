dd2ccca4cc5014add7cd3799137fcc16c8d8f09c
ojeagaclifford <cli24003@byui.edu>
2025-11-03 10:24:01 +0100

    I uppate the addProductTocart function to be able to add local storeage item in list format.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cartItems = getLocalStorage('so-cart') || [];
+
+  // console.log(cartItems);
+
+  cartItems.push(product);
+  setLocalStorage('so-cart', cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -13,6 +18,14 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const addbottonText = document.getElementById('addToCart');
+
+document.getElementById('addToCart').addEventListener('click', () => {
+  addToCartHandler;
+  // I modefily  the text content of the add item button to change to item add so it can be notice that the item have been added
+  addbottonText.textContent = 'Item added ✅';
+  // I updated the and  return the button  text content back to "Add to cart" in 2s
+  setTimeout(() => {
+    addbottonText.textContent = 'Add to cart';
+  }, 2000);
+});
